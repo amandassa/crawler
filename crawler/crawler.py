64d8083e9168561257d1e6d95db4eaf05b667c57
Amanda <amandassa504@gmail.com>
2023-03-14 11:28:23 -0300

    Add files via upload

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 import urllib.request
 from bs4 import BeautifulSoup
 seeds = []
-with open('index.html') as fp:
+with open('index.html', encoding='utf-8') as fp:
   soup = BeautifulSoup(fp, 'lxml')
 
 #print("Título:", soup.title.string)
@@ -15,15 +16,17 @@ for opt in soup.find_all('option'):
     nome: opt.string
   }
   seeds.append(linha)
-  print(linha)
+  # print(linha)
 
   #print("value: ", img.attrs.get("value")+'- '+ img.string +'\n')
-'''
+
 browser = webdriver.Chrome()
 browser.get('http://www.agerba2.ba.gov.br/transporte/localidade_linha.asp')
 element = browser.find_element(By.NAME, "sel_linha")
 options = element.find_elements(By.TAG_NAME, "option")
-for i in options:
-  print('SELENIUM!!!! ----: ',i.string)
-  break
-'''
+
+select = Select(driver.find_element_by_id('fruits01'))
+
+select.select_by_value(seeds[0]._id)
+
+browser.find_element_by_name('submit').click()
